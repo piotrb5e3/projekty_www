4d6a221bdad484d1c11692b0311c8d6395e98a07
Piotr Bakalarski <pb358995@red15.mimuw.edu.pl>
2016-05-12 18:34:29 +0200

    split actual editing into separate selenium test, added some timed waits so it doesn/t crash on slow systems

--- zad1/pkw2005/selenium_tests/pkw_gmina_edit.py
+++ zad1/pkw2005/selenium_tests/pkw_gmina_edit.py
@@ -15,20 +15,9 @@ def open_lubuskie_edit(d):
     e = d.find_element_by_xpath('//*[@id="edittab"]/tbody/tr[1]/th[1]')
     assert "Kandydat" in e.text
 
-def editform_input(d, nk1, nk2):
-    e = d.find_element_by_xpath('//*[@id="id_k1"]')
-    e.clear()
-    e.send_keys(nk1)
-    e = d.find_element_by_xpath('//*[@id="id_k2"]')
-    e.clear()
-    e.send_keys(nk2)
-
-def editform_submit(d):
-    d.find_element_by_xpath('//*[@id="geditid"]/button[1]').click()
-
 try:
     driver = webdriver.Firefox()
-    driver.implicitly_wait(3)
+    driver.implicitly_wait(10)
 
     driver.get("http://localhost:8000")
 
@@ -46,26 +35,7 @@ try:
 
     login(driver, "alice", "bobbobbob")
     open_lubuskie_edit(driver)
-
-    # Test for edit functionality
-
-    login(driver, "alice", "bobbobbob")
-    open_lubuskie(driver)
-    e = driver.find_element_by_xpath('//*[@id="seemore"]/tbody/tr[3]/td[2]')
-    k1 = e.text
-    e = driver.find_element_by_xpath('//*[@id="seemore"]/tbody/tr[3]/td[3]')
-    k2 = e.text
-    driver.find_element_by_xpath('//*[@id="seemore"]/tbody/tr[3]/td[4]/a').click()
-    editform_input(driver, k2, k1)
-    editform_submit(driver)
-    a = driver.switch_to_alert()
-    a.accept()
-    open_lubuskie(driver)
-    e = driver.find_element_by_xpath('//*[@id="seemore"]/tbody/tr[3]/td[2]')
-    assert k2 in e.text
-    e = driver.find_element_by_xpath('//*[@id="seemore"]/tbody/tr[3]/td[3]')
-    assert k1 in e.text
-   
+  
 finally:
     driver.close()
 
